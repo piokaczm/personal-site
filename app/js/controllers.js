9eb762da5b203c4e063a9f791e50fb360d2ec743
Piotr Kaczmarczyk <piokaczm@gmail.com>
2015-09-01 00:35:08 +0200

    Move navbar links to controller, add navbar hyperlinks, remove a:focus and a:active outline

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,5 +3,7 @@
 var personalSiteController = angular.module('personalSiteControllers', []);
 
 personalSite.controller("NavController", ['$scope', function($scope) {
+  
+  $scope.links = ['home', 'cv', 'projects', 'works', 'contact'];
 
 }]);
