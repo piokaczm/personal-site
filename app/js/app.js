8459b0dd9091f5f6549f6a73e135e7dffa6f032d
Piotr Kaczmarczyk <piokaczm@gmail.com>
2015-09-04 14:27:37 +0200

    Finish ng-view animations

--- app/js/app.js
+++ app/js/app.js
@@ -3,6 +3,7 @@
 // Declare app level module which depends on views, and components
 var personalSite = angular.module('personalSite', [
   'ngRoute',
+  'ngAnimate',
   'personalSiteControllers',
   'personalSiteDirectives',
   'personalSiteServices'
